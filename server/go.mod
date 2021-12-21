03ed18ec74eca7ddb5ebf404bd80086fd08fc755
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2021-12-21 22:24:13 +0000

    Added go setup

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module server
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/lib/pq v1.10.4
+)
---
+++ server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	/* "github.com/gin-gonic/gin"
+	   "github.com/gin-contrib/cors"
+	   _ "github.com/lib/pq" */)
+
+func main() {
+	fmt.Println("Under Construction...")
+}
