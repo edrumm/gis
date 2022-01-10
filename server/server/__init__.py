f2a77508e23d4ec2395c035882356c872169f235
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2022-01-10 22:58:43 +0000

    successful postgres connection

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,4 +1,4 @@
-import signal
+import os, signal, psycopg2
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,21 +6,32 @@ app = Flask(__name__)
 import server.database as db
 
 
+# DB init
+try:
+   conn, cur = db.connect()
+
+except (Exception, psycopg2.Error) as err:
+        print('Error connecting to Postgres:', err)
+        exit(1)
+
+
 @app.route('/')
 def hello_world():
-   return db.test()
+   return "Under Construction..."
 
 
-def stop():
-   pass
+# Testing ONLY
+# Load sample vector dataset from PostGIS
+@app.route('/test')
+def test():
+   cur.execute("""SELECT name, ST_AsText(geom) FROM nyc_subway_stations""")
+   points = cur.fetchall()
+
+   return str(points)
 
 
 if __name__ == '__main__':
-   signal.signal(signal.SIGTERM, stop)
-   signal.signal(signal.SIGINT, stop)
-   signal.signal(signal.SIGKILL, stop)
-
-   # Read env
-   # conn, cur = db.connect()
+   
+   # TODO
 
    app.run()
--- server/server/database.py
+++ server/server/database.py
@@ -1,13 +1,16 @@
 import psycopg2 as pg
-
-
-def test():
-    return 'Hello!'
+from dotenv import dotenv_values
 
 
 def connect():
-    # Read credentials from .env
-    conn = pg.connect('')
+    config = dotenv_values('.env')
+
+    pg_user = config.get('PG_USER')
+    pg_host = config.get('PG_HOST')
+    pg_password = config.get('PG_PASSWORD')
+    pg_db = config.get('PG_DB')
+
+    conn = pg.connect(host=pg_host, database=pg_db, user=pg_user, password=pg_password)
     cur = conn.cursor()
 
     return conn, cur
